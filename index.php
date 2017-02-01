18fc4926fa92507222ffd914eef6d0e36add0b40
Georg Rokita <rokita.georg@freenet.de>
2017-02-01 13:16:51 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: georgrokita
+ * Date: 01.02.17
+ * Time: 10:12
+ */?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Zahnarztpraxis Rokita</title>
+
+    <scrip type="text/javascript" src="js/bootstrap.js"></scrip>
+    <scrip type="text/javascript" src="js/bootstrap.min.js"></scrip>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/desktop.css">
+    <link rel="stylesheet" href="css/mobile.css">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.css.map">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css.map">
+    <link rel="stylesheet" href="css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.css.map">
+</head>
+<body>
+
+<header>
+    <div class="mobil_menu">
+        <div class="h_menu"></div>
+        <div class="i_menu"><span>Home</span></div>
+        <div class="i_menu"><span>Praxis</span></div>
+        <div class="i_menu"><span>Team</span></div>
+        <div class="i_menu"><span>Zahnarztliche Leistungen</span></div>
+        <div class="i_menu"><span>Jobs</span></div>
+        <div class="i_menu"><span>Kontakt</span></div>
+        <div class="i_menu"><span><a href="tel:+4935525908">0355/25908</a></span></div>
+        <div class="i_menu"><span>Thiemstraße 124, 03050 Cottbus</span></div>
+    </div>
+    
+</header>
+
+
+
+
+</body>
+</html>
