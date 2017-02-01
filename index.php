89ec28d9d7d59eeb96b3c8e21a4c3c4dd64eaaee
Georg Rokita <rokita.georg@freenet.de>
2017-02-02 00:55:07 +0100

    in Mobil Version -Header -Nav -Contentboxes ready

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * User: georgrokita
  * Date: 01.02.17
  * Time: 10:12
- */?>
+ */ ?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -16,35 +16,112 @@
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/desktop.css">
-    <link rel="stylesheet" href="css/mobile.css">
-
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/bootstrap.css.map">
     <link rel="stylesheet" href="css/bootstrap-grid.css">
     <link rel="stylesheet" href="css/bootstrap-grid.css.map">
     <link rel="stylesheet" href="css/bootstrap-reboot.css">
     <link rel="stylesheet" href="css/bootstrap-reboot.css.map">
+
+
+    <link rel="stylesheet" href="css/desktop.css">
+    <link rel="stylesheet" href="css/mobile.css">
+
 </head>
 <body>
 
 <header>
-    <div class="mobil_menu">
-        <div class="h_menu"></div>
-        <div class="i_menu"><span>Home</span></div>
-        <div class="i_menu"><span>Praxis</span></div>
-        <div class="i_menu"><span>Team</span></div>
-        <div class="i_menu"><span>Zahnarztliche Leistungen</span></div>
-        <div class="i_menu"><span>Jobs</span></div>
-        <div class="i_menu"><span>Kontakt</span></div>
-        <div class="i_menu"><span><a href="tel:+4935525908">0355/25908</a></span></div>
-        <div class="i_menu"><span>Thiemstraße 124, 03050 Cottbus</span></div>
+    <nav>
+        <div class="nav_h_mobil_box">
+            <a href="#">
+                <div class="h_menu"><span class="nav_icons hidden">&#9776</span><span
+                        class="nav_icons active">&#10006;</span></div>
+            </a>
+        </div>
+        <div class="nav_item_box">
+            <a href="#">
+                <div class="i_menu"><span>Home</span></div>
+            </a>
+            <a href="#">
+                <div class="i_menu"><span>Praxis</span></div>
+            </a>
+            <a href="#">
+                <div class="i_menu"><span>Team</span></div>
+            </a>
+            <a href="#">
+                <div class="i_menu"><span>Zahnarztliche Leistungen</span></div>
+            </a>
+            <a href="#">
+                <div class="i_menu"><span>Jobs</span></div>
+            </a>
+            <a href="#">
+                <div class="i_menu"><span>Kontakt</span></div>
+            </a>
+            <a href="tel:+4935525908">
+                <div class="i_menu"><span class="nav_infoData">&#9990; 0355 25 908</span></div>
+            </a>
+            <a href="#">
+                <div class="i_menu"><span class="nav_infoData">&#8962; Thiemstraße 124, 03050 Cottbus</span></div>
+            </a>
+        </div>
+    </nav>
+
+</header>
+<div class="header_img_box">
+    <img src="img/teaser_1.jpg" alt="Bild von der Zahnarztpraxis">
+</div>
+<div class="logo_wrapper">
+    <div class="logo_items">
+        <span>&#9990; 0355 25 908</span>
+    </div>
+    <div class="logo_items">
+        <img src="img/Logo_ZAP_black.png" alt="Logo Zahnarztpraxis Rokita">
+    </div>
+    <div class="logo_items">
+        <span>
+             &#8962; Thiemstraße 124, 03050 Cottbus
+        </span>
+    </div>
+</div>
+
+<div class="welcome_wrapper">
+
+    <div class="welcome_text_box">
+        <h2>
+            Herzlich Willkommen
+        </h2>
+        <h4>
+            in der Zahnarztpraxis Rokita.
+        </h4>
+        <p>
+            Vielen Dank für Ihr Interesse und Ihr Vertrauen in unsere Praxis.
+        </p>
+        <p>
+            Frau Dr. med. Veronika Rokita und die Zahnärztin Susann Rokita, sowie das gesamte Team freuen sich Sie als Patient begrüßen zu dürfen. Der Patient steht bei uns im Mittelpunkt, so dass wir eine individuell auf Sie angepasste zahnmedizinische Versorgung bieten können.
+            Unser Ziel ist es in einer angenehmen Atmosphäre Ängste und Schmerzen zunehmen und dem Patienten zu einem strahlenden Lächeln zu verhelfen.
+        </p>
+    </div>
+</div>
+
+
+<div class="akuelles_wrapper">
+    
+    <div class=" akt_banner_box">
+        <h4>Aktueller Hinweis für unsere Partienten </h4>
+    </div>
+    <div class="akl_box">
+
+        <div class="akt_headline_box">
+            <h4>Urlaubsankündigung</h4>
+        </div>
+        
+        <div class="akt_content_box">
+            <p>Wir haben Urlaub vom 1. Jan. - 31. Dez.. Bitte kommen sie uns nächstest Jahr besuchen.</p>
+            <p>Ihre ZAP Rokita</p>    
+        </div>
+        
     </div>
     
-</header>
-
-
-
-
+</div>
 </body>
 </html>
