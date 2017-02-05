bbee29dea4197e3a7f8ac2907d07719cdd7b440d
Georg Rokita <rokita.georg@freenet.de>
2017-02-05 19:10:42 +0100

    template done

---
+++ production/js/app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by georgrokita on 05.02.17.
+ */
+function startZAPApp() {
+
+    alert("Hello");
+
+
+}
+
+/*
+$(document).ready(function(){
+
+    $( ".h_menu" ).click(function() {
+
+        alert("Hello2");
+        $( "#nav_menu_icon" ).fadeToggle( "slow", "linear" ).delay(50);
+        $( "#nav_closing_icon" ).fadeToggle( "slow", "linear" );
+    });
+
+});
