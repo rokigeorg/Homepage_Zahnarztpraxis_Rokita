be3d197888ff9cab4550c9bd71f99ff22ec33f64
Georg <rokita.georg@freenet.de>
2018-05-23 23:57:20 +0200

    update website to new DSGVO Datenschutz and adjusted the gulp script as well as the README

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,27 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 
-gulp.task('style', function() {
+gulp.task('style', function () {
     // place code for your default task here
 
-    gulp.src('./scss/*.scss',['scss'])
+    gulp.src('./scss/*.scss', ['scss'])
         .pipe(sass())
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.reload({stream:true}));
+        .pipe(gulp.dest('./production/css'))
+        .pipe(browserSync.reload({stream: true}));
 
 });
 
-gulp.task('serve',function () {
+gulp.task('serve', function () {
 
-    browserSync.init({server:{
-        baseDir:'./'
-    }});
+    browserSync.init({
+        server: {
+            baseDir: './production/'
+        }
+    });
 
     gulp.watch('./scss/*.scss', ['style']);
-    gulp.watch('*.php').on('change',browserSync.reload());
+    gulp.watch('production/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default',['style','serve']);
+gulp.task('default', ['style', 'serve']);
 
